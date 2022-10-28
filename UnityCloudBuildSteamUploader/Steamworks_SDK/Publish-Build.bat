5ff95b532d26e86eabc15a421f17cbe75bc37fab
Alex Schearer <aschearer@gmail.com>
2022-10-28 13:11:27 -0700

    Dropping DRM which fails.

--- UnityCloudBuildSteamUploader/Steamworks_SDK/Publish-Build.bat
+++ UnityCloudBuildSteamUploader/Steamworks_SDK/Publish-Build.bat
@@ -4,4 +4,4 @@ set password=%2
 set appId=%3
 set appScript=%4
 set appExe=%5
-"%~dp0\builder\steamcmd" +login %username% %password% +drm_wrap %appId% "%appExe%" "%appExe%" drmtoolp 0 +run_app_build ..\scripts\%appScript% +quit
+"%~dp0\builder\steamcmd" +login %username% %password% %appId% "%appExe%" "%appExe%" +run_app_build ..\scripts\%appScript% +quit
